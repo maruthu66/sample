49880171706f5efbc5a6b3be044db80c6aecdcdb
maruthu66 <pandimaruthu07@gmail.com>
2020-03-11 10:08:58 +0530

    my sample project

---
+++ src/firstproject/Sample.java
@@ -0,0 +1,12 @@
+package firstproject;
+
+public class Sample {
+	public void first() {
+		System.out.println("output sucessfull");
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.first();
+	}
+
+}
